28cf4822ad0235ddd63c1f9c9ec9ed426c56b5e1
Greg Sharp <gregsharp.geo@yahoo.com>
2015-04-02 09:57:34 -0400

    Initial skeleton

---
+++ src/main.cxx
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "stdio.h"
+
+int
+main (int argc, char *argv[])
+{
+    printf ("Hello world\n");
+    return 0;
+}
---
+++ src/yaml_loader.cxx
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "stdio.h"
+
+int
+yaml_loader (int argc, char *argv[])
+{
+    printf ("Hello world (from yaml_loader)\n");
+    return 0;
+}
